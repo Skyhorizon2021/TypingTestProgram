1fd9711e68ca179a80e29fd4d77345606562d945
Skyhorizon2021 <me@skyhorizon2021.com>
2023-05-10 13:09:29 -0700

    print output

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
-word_List = []
+all_word_List = []
 with open("words_alpha.txt","r") as wordList:
     for line in wordList:
-        word_List.extend(line.split())
-print(word_List)
+        all_word_List.extend(line.split())
+print(all_word_List)
+
+
         
